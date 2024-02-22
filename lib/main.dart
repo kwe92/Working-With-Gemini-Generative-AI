95754dc3f1e1cffb7b2c5002c5e1004ef77a471f
Kwe <Kweayon@gmail.com>
2024-02-21 23:53:45 -0600

    feat: main dart file

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:working_with_gemini/feat/deeperMeanings/ui/deeper_meanings_view.dart';
+import 'package:working_with_gemini/load_env_variables.dart';
+
+void main() async {
+  await loadEnvVariables();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          background: Color.fromARGB(72, 158, 158, 158),
+        ),
+      ),
+      home: const DeeperMeaningsView(),
+    );
+  }
+}
