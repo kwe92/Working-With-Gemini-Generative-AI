11b0fbc4f369d9d2f5ad5d01df74b0c2468befcd
Kwe <Kweayon@gmail.com>
2024-02-23 18:49:58 -0600

    feat: used svg image for prompt card

--- lib/feat/deeperMeanings/ui/widgets/prompt_card.dart
+++ lib/feat/deeperMeanings/ui/widgets/prompt_card.dart
@@ -8,38 +8,84 @@ class PromptCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: refactor with svg image
-    final svgImage = SvgPicture.asset("/Users/kwe/WorkingWithGemini/working_with_gemini/assets/lotus-flower-bloom.svg");
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(16),
-          ),
-          image: DecorationImage(
-            image: Image.asset("/Users/kwe/WorkingWithGemini/working_with_gemini/assets/compassion.avif").image,
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 6, bottom: 8, right: 6),
-              child: Text(
-                promptText,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  // color: Colors.purple,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w800,
-                ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background.withOpacity(0.20),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(16),
               ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.15),
+                  offset: Offset(0, 2),
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                ),
+              ],
             ),
-          ],
-        ),
+            child: SvgPicture.asset(
+              "/Users/kwe/WorkingWithGemini/working_with_gemini/assets/lotus-flower-bloom.svg",
+              // color: Colors.deepOrange[200],
+              // color: Colors.deepPurple[200],
+              // color: Colors.deepPurple[300],
+              color: Colors.deepPurpleAccent[100],
+              // color: Colors.pink[100],
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 6, bottom: 16, right: 6),
+                  child: Text(
+                    promptText,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+//  Container(
+//         decoration: BoxDecoration(
+//           borderRadius: const BorderRadius.all(
+//             Radius.circular(16),
+//           ),
+//           image: DecorationImage(
+//             image: Image.asset("/Users/kwe/WorkingWithGemini/working_with_gemini/assets/compassion.avif").image,
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.end,
+//           children: [
+//             Padding(
+//               padding: const EdgeInsets.only(left: 6, bottom: 8, right: 6),
+//               child: Text(
+//                 promptText,
+//                 textAlign: TextAlign.center,
+//                 style: const TextStyle(
+//                   // color: Colors.purple,
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.w800,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
